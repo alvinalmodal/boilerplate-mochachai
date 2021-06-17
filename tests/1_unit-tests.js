6b841bcba669e6dcb5b051589e77f105a226f038
alvin almodal <almodalalvin@gmail.com>
2021-06-17 12:06:50 +0800

    added test for isdefined and isundefined

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -11,13 +11,14 @@ suite("Unit Tests", function () {
       );
       assert.isNotNull(1, "1 is not null");
     });
+    // #2
+    test("#isDefined, #isUndefined", function () {
+      assert.isDefined(null, "null is not undefined");
+      assert.isUndefined(undefined, "undefined IS undefined");
+      assert.isDefined("hello", "a string is not undefined");
+    });
   });
-  //   // #2
-  //   test("#isDefined, #isUndefined", function () {
-  //     assert.fail(null, "null is not undefined");
-  //     assert.fail(undefined, "undefined IS undefined");
-  //     assert.fail("hello", "a string is not undefined");
-  //   });
+
   //   // #3
   //   test("#isOk, #isNotOk", function () {
   //     assert.fail(null, "null is falsey");
